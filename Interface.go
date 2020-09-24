e1514cac148798278249802bbbae6cc7fd8044d2
HoYoungChun <ho723@naver.com>
2020-09-24 12:09:20 +0900

    인터페이스(Interface)에 대한 예제

--- Interface.go
+++ Interface.go
@@ -20,6 +20,18 @@ func (c circle) area() float64 {
   return math.Pi * c.radius * c.radius
 }
 
+func measure(g geometry) {
+  fmt.Println(g)
+  fmt.Println(g.area())
+}
+
+func main() {
+  r := rect{20.0, 25.0}
+  c := circle{32.0}
+  measure(r)
+  measure(c)
+}
+
 /*
   ✓ 메서드의 집합을 정의한 타입이다. 객체지향 언어에서 부모 클래스와 동일한 개념으로 이용가능하다.
   ✓ Go 언어에서는 인터페이스의 메서드만 구현된 구조체라면 모두 동일한 인터페이스로 취급할 수 있는데 이를 덕 타이핑 이라고 부른다.
