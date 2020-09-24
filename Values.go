cd023e806e20b817f13244bdc9e2581699f5bc42
HoYoungChun <ho723@naver.com>
2020-09-24 11:11:39 +0900

    값(Values)에 대한 예제

--- Values.go
+++ Values.go
@@ -1,14 +1,18 @@
 package main
+
 import "fmt"
+
 func main() {
-fmt.Println("se" + "oul")
-fmt.Println("1 + 1 =", 1+1)
-fmt.Println("9 * 9 =", 9*9)
-fmt.Println("7.0/3.0 =", 7.0/3.0)
-fmt.Println(true && false)
-fmt.Println(true || false)
-fmt.Println(!true)
-fmt.Println(nil)
+  fmt.Println("se" + "oul")
+  
+  fmt.Println("1 + 1 =", 1+1)
+  fmt.Println("9 * 9 =", 9*9)
+  fmt.Println("7.0/3.0 =", 7.0/3.0)
+  
+  fmt.Println(true && false)
+  fmt.Println(true || false)
+  fmt.Println(!true)
+  fmt.Println(nil)
 }
 
 /*
