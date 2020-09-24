f8bd3a88c3f0efb0d4788ca9bff2a171df68aacf
HoYoungChun <ho723@naver.com>
2020-09-24 11:12:32 +0900

    변수(Variable)에 대한 예제1

--- Variable_1.go
+++ Variable_1.go
@@ -1,14 +1,19 @@
 package main
+
 import "fmt"
+
 func main() {
-var a int = 10
-fmt.Println("a =", a)
-var b = true // 타입 추론
-fmt.Println(b)
-var c int
-fmt.Println(c)
-d := "seoul" // 짧은 표현
-fmt.Println(d)
+  var a int = 10
+  fmt.Println("a =", a)
+  
+  var b = true // 타입 추론
+  fmt.Println(b)
+  
+  var c int
+  fmt.Println(c)
+  
+  d := "seoul" // 짧은 표현
+  fmt.Println(d)
 }
 
 /*
