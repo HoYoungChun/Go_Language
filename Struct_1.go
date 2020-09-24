70b54b8e75274dfe98db8b73e62411aa17046e29
HoYoungChun <ho723@naver.com>
2020-09-24 12:02:58 +0900

    구조체(Struct)에 대한 예제1

--- Struct_1.go
+++ Struct_1.go
@@ -12,3 +12,9 @@ func main() {
   fmt.Println(rct1)
   fmt.Println(rct2)
 }
+
+/*
+  ✓ 서로 다른 타입을 가진 필드를 하나의 데이터 타입으로 정의할 때 구조체를 이용한다.
+  ✓ Go 언어에서는 구조체를 이용해 객체지향 기법을 적용 할 수 있다.
+  ✓ type [name] struct { fields } 의 형식으로 구조체를 선언한다.
+*/
